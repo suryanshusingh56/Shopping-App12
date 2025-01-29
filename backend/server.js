fbfa495089f77a63f0d5c3531eba8d90e9ad7f2a
suryanshusingh56 <suryanshusingh56@gmail.com>
2025-01-29 13:14:26 +0530

    17th

--- backend/server.js
+++ backend/server.js
@@ -14,18 +14,7 @@ connectDb();
 app.use(express.json());
   const cors = require('cors');
 
-  app.use(
-    cors({
-      origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      credentials: true,
-    })
-)
+app.use(cors());
   
   
 
